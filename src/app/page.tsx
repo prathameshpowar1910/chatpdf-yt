6b592106564448467cfaa5b1ef100cdc72c4a949
Prathamesh Powar <ppowar1910@gmail.com>
2023-09-20 22:47:07 +0530

    set up drizzzle orm with neon db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import {LogInIcon} from "lucide-react";
 
 export default async function Home() {
 
-  const {userId} = await auth()
-  const isAuth = !!userId
+  const {userId} = await auth();
+  const isAuth = !!userId;
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-200 to-teal-200 ">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
